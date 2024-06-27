1e731f2e5d638c7d80df58b0dda1ade8ac2f4e1b
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-06-27 02:34:10 -0300

    avançei no layout da popup dos filmes populares

--- src/app/app/filmes-populares/page.tsx
+++ src/app/app/filmes-populares/page.tsx
@@ -25,6 +25,7 @@ import {
 
 type Movie = {
  id: number;
+ backdrop_path: string;
  poster_path: string;
  title: string;
  overview: string;
@@ -39,7 +40,7 @@ export default function FilmesPopulares() {
   let config = {
    method: "GET",
    endPoint: "/movie/popular",
-   params: { language: "pt-br", page: "1" },
+   params: { language: "pt-br", page: "43" },
   };
 
   await axios
@@ -82,20 +83,45 @@ export default function FilmesPopulares() {
               className="w-[100%] h-[100%] transition sm:hover:scale-105 mb-4 rounded-xl object-cover"
              />
             </DialogTrigger>
-            <DialogContent className="flex flex-col max-h-screen overflow-auto items-center sm:max-w-[80%]">
-             <DialogHeader className="grid grid-cols-1 max-h-fit justify-items-center text-start sm:flex sm:flex-row sm:items-center sm:gap-4">
+            <DialogContent
+             style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://image.tmdb.org/t/p/original${movie.backdrop_path}')`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+             }}
+             className="flex flex-col h-fit max-h-screen overflow-auto items-center sm:max-w-[80%] sm:max-h-[90%] text-white border-none sm:rounded-xl"
+            >
+             <DialogHeader className="grid grid-cols-1 max-h-fit justify-items-center text-start sm:flex sm:flex-row sm:items-start sm:gap-4">
               <img
                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                alt="Image"
-               className="w-[200px] h-auto mb-4 rounded-xl object-cover transition sm:animate-none sm:mb-0"
+               className="w-[200px] h-auto mb-4 rounded-xl object-cover shadow-xl shadow-black transition sm:animate-none sm:mb-0"
               />
-              <div>
-               <DialogTitle className="pb-2">{movie.title}</DialogTitle>
+              <div className="!m-0 text-white h-[100%]">
+               {movie.backdrop_path ? (
+                <img
+                 src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                 alt="Image"
+                 className="hidden w-[98%] h-[200px] mb-4 rounded-xl object-cover shadow-xl shadow-black transition md:block sm:animate-none sm:mb-0"
+                />
+               ) : (
+                <div className="flex h-screen">
+                 <LoadBox />
+                </div>
+               )}
+               <DialogTitle className="pt-6 pb-1 sm:text-4xl">{movie.title}</DialogTitle>
                Sinopse:
-               <DialogDescription className="inline ml-1">{movie.overview}</DialogDescription>
+               <DialogDescription className="inline ml-1 text-white">
+                {movie.overview}
+               </DialogDescription>
               </div>
              </DialogHeader>
-             <div>Mais conteúdo - Possível trailer</div>
+             <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam unde veritatis corporis
+              perspiciatis quas explicabo veniam officia dolor placeat quaerat id at facilis sunt
+              commodi labore neque, odit blanditiis ipsam.
+             </div>
             </DialogContent>
            </Dialog>
 
@@ -105,7 +131,7 @@ export default function FilmesPopulares() {
            <p>Lançamento: {formatReleaseDate(movie.release_date)}</p>
            <p>
             Avaliação:
-            <span className="bg-primary text-white shadow-sm shadow-neutral-500 px-2 ml-1 rounded">
+            <span className="bg-primary text-white shadow-sm shadow-neutral-500 px-3 ml-1 rounded">
              {(Math.round(movie.vote_average * 10) / 10).toFixed(1)}
             </span>
            </p>
